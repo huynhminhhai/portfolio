b9b866efb698d79c3bf2ec3a3dbe33dbc35eb089
Huynh Minh Hai <huynhminhhai.1555@gmail.com>
2025-03-14 15:49:37 +0700

    shinra tensei

---
+++ assets/js/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+  $('.project-slider').slick({
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    nextArrow: $('#project-next')
+  });
+
+  $('.project-slider').on('init reInit afterChange', function (event, slick) {
+    $('.slick-slide').each(function () {
+      if ($(this).hasClass('slick-active')) {
+        $(this).removeAttr('aria-hidden').removeAttr('inert');
+      } else {
+        $(this).attr('inert', '');
+      }
+    });
+  });
+
+  let lastScrollY = $(window).scrollTop();
+
+  $(window).on("scroll", function () {
+    let currentScrollY = $(window).scrollTop();
+
+    if (currentScrollY > lastScrollY) {
+      $("header").addClass("hide"); // Ẩn header khi scroll xuống
+    } else {
+      $("header").removeClass("hide"); // Hiện header khi scroll lên
+    }
+
+    lastScrollY = currentScrollY;
+  });
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.utils.toArray("section").forEach((section, index) => {
+    gsap.from(section, {
+      opacity: 0,
+      y: 50,
+      duration: 1,
+      scrollTrigger: {
+        trigger: section,
+        start: "top 80%",
+        end: "top 30%",
+        toggleActions: "play none none reverse"
+      }
+    });
+  });
+
+});
